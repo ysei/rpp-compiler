1e69a935bf8117fabe38bb118629a44195b51c27
Sergiy Dubovik <sergiy.dubovik@nokia.com>
2011-05-06 22:11:58 +0300

    Added global files

---
+++ global.cpp
@@ -0,0 +1,6 @@
+#include "global.hpp"
+
+namespace rpp
+{
+bool doNewLine = false;
+};
---
+++ global.hpp
@@ -0,0 +1,9 @@
+#ifndef GLOBAL_H__
+#define GLOBAL_H__
+
+namespace rpp
+{
+        extern bool doNewLine;
+};
+
+#endif //  GLOBAL_H__
