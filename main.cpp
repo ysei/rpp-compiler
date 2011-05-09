53907feba673331375731cb7f593eb1dbc7dcf82
Sergiy Dubovik <sergiy.dubovik@nokia.com>
2011-05-09 08:37:31 +0300

    Added example of possible source file

--- main.cpp
+++ main.cpp
@@ -10,13 +10,29 @@ extern int yyparse();
 extern NBlock * programBlock;
 void scan_buffer(const char * buf);
 
+/*
+def func(x, y)
+    return x + y
+end
+
+def main
+    k = func(10, 2)
+    puts k
+
+    p = 23 + 43 * (10 + k)
+    puts k
+end
+
+
+*/
 int main(int argc, char * argv[])
 {
     argc = argc;
     argv = argv;
     InitializeNativeTarget();
     //scan_buffer("if 20 + 30 begin 2 + 3 else 10 + 20 end");
-    scan_buffer("if 0 begin 30 else 40 end");
+    //scan_buffer("if 0 begin 30 else 40 end");
+    scan_buffer("x = 10");
     yyparse();
     cout << programBlock << endl;
     CodeGenContext context;
