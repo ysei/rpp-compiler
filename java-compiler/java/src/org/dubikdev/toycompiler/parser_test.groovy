4adefd03b730f1e2334da78b4bbcdce386eed9ba
Sergiy Dubovik <sergiy.dubovik@nokia.com>
2011-09-21 22:10:06 +0300

    Added toArray function to convert tree into array of arrays

--- java-compiler/java/src/org/dubikdev/toycompiler/parser_test.groovy
+++ java-compiler/java/src/org/dubikdev/toycompiler/parser_test.groovy
@@ -4,7 +4,9 @@ import org.antlr.runtime.ANTLRStringStream
 import org.antlr.runtime.CommonTokenStream
 import org.antlr.runtime.RecognitionException
 import org.antlr.runtime.tree.CommonTree
-import org.dubikdev.toycompiler.rppLexer
+
+import org.antlr.runtime.Token
+import static org.dubikdev.toycompiler.rppLexer.*
 
 class GroovyParserTest extends GroovyTestCase {
 
@@ -25,12 +27,64 @@ class GroovyParserTest extends GroovyTestCase {
         def tree = parse(code);
         def token = tree.getToken();
 
-        assertEquals(token.getType(), rppLexer.FUNC_DEF);
+        assertEquals(token.getType(), FUNC_DEF);
+
+        def ar = toArray(tree)
 
         def children = tree.getChildren();
         assertEquals("func", children[0].getToken().getText())
-        assertEquals(rppLexer.ARG_DEF, children[1].getToken().getType())
-        assertEquals(rppLexer.BLOCK, children[2].getToken().getType())
+        assertEquals(ARG_DEF, children[1].getToken().getType())
+        assertEquals(BLOCK, children[2].getToken().getType())
+    }
+
+    List toArray(CommonTree tree) {
+        def array = []
+        return toArrayRecursively(tree)
+    }
+
+    class TokenInfo {
+        public String text
+        public int type
+        String toString() {
+            return text + ", " + type
+        }
+    }
+
+    List toArrayRecursively(CommonTree tree) {
+        def array = []
+        array.add(getTokenInfo(tree.getToken()))
+
+        if (tree.getChildren() != null) {
+            for (child in tree.getChildren()) {
+                def children = []
+                children = toArrayRecursively(child)
+                array.add(children)
+            }
+        }
+
+        return array
+    }
+
+    TokenInfo getTokenInfo(Token token) {
+        TokenInfo info = new TokenInfo()
+        info.text = token.getText()
+        info.type = token.getType()
+        return info
+    }
+
+    // x + y
+    // [+, [x, y]]
+
+    public void testSimpleExpressionTwoNumbersAdd() {
+        def code = """
+        10 + 5
+        """
+
+        def tree = parse(code)
+        assertNotNull("Was not able to parse following code: " + code, tree)
+        def token = tree.getToken()
+
+        assert true
     }
 
     private CommonTree parse(String inputString) throws RecognitionException {
